d45a53f2a6203923b902d9f258eb2a5fd856d338
RusheenB <94388849+RusheenB@users.noreply.github.com>
2022-01-19 03:30:14 +0000

    Update labour map.py

--- map visualisation/labour map.py
+++ map visualisation/labour map.py
@@ -31,6 +31,6 @@ fig.update_layout(width=800,
     margin=dict(t=0, b=0, l=0, r=0))
 fig.show()
 
-# generate html file to embed
+# generate html file to upload on github and then embed
 import plotly.io as pio
 pio.write_html(fig, file='LABOUR MAP.html', auto_open=True)
